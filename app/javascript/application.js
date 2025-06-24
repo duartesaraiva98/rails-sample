18bd5d964ef16ae929e55e7137fdab2b112fe0d8
Duarte Saraiva <saraiva@zorgdomein.nl>
2025-06-24 14:10:33 +0100

    Add faro web sdk

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,24 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import {initializeFaro} from '@grafana/faro-web-sdk';
+
+const faroEndpoint = document
+    .querySelector('meta[name="faro-endpoint"]')
+    ?.getAttribute('content');
+
+const apiKey = document
+    .querySelector('meta[name="faro-api-key"]')
+    ?.getAttribute('content');
+
+const appName = document
+    .querySelector('meta[name="app-name"]')
+    ?.getAttribute('content');
+
+initializeFaro({
+    url: faroEndpoint,
+    apiKey: apiKey,
+    app: {
+        name: appName,
+    }
+});
